cbdebd97c69e8d21c576bdc153bddbb6580f4f7c
sergicalsix <kenp3434membertip1@gmail.com>
2021-08-29 15:23:47 +0900

    add: page icons

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ st.set_page_config(
    page_icon=":cat:",
  )
 
-st.title("超スケジューリングくん")
+st.title("超スケジューリングくん　(東京エリア)")
 st.write('<style>h1 {color: green;}</style>', unsafe_allow_html=True)
 
 st.markdown('<style>' + open('icons.css').read() + '</style>', unsafe_allow_html=True)
-st.markdown('<i class="material-icons">face</i>', unsafe_allow_html=True)
+#st.markdown('<i class="material-icons">face</i>', unsafe_allow_html=True)
+
+
+st.image('img/icons/sch.png')
 
 #st.image('<i class="material-icons"> ":face:" </i>', unsafe_allow_html=True)
 
@@ -24,12 +27,14 @@ st.markdown('<i class="material-icons">face</i>', unsafe_allow_html=True)
 #    ('東京','大阪')
 #)
 area_option = "東京"
+
+st.sidebar.image('img/icons/flag.png')
 user_option = st.sidebar.radio(
     "属性を選択してください",
-    ('運転手','管理者')
+    ('セールスドライバー','センター長')
 )
 
-if user_option == '運転手':
+if user_option == 'セールスドライバー':
     name = st.sidebar.radio(
         "氏名",
         ('大久保','Chou','渋谷','渡辺','高橋')
@@ -48,27 +53,30 @@ st.sidebar.markdown("""-----""")
 go = st.sidebar.checkbox('実行!!')
 
 if go == True :
-    if user_option == '運転手':
+    if user_option == 'セールスドライバー':
         #ガントチャートの描画
+        st.warning(f'本日の{name}さんの配達予定です！！')
+#st.info('Info message')
+#st.success('Success message')
         fig = my_gantt_chart(name = name)
         st.plotly_chart(fig)
         #地図の描画
 
-        st.write(f"{name}さんに配達をお願いしている場所です！本日はよろしくお願いいたします！")
+        st.info(f"{name}さんに配達をお願いしている場所です！本日はよろしくお願いいたします！")
         fig = my_task_view(name = name)
         st.pydeck_chart(fig)
 
-        st.write("配送場所")
+
+        st.success("配送場所")
         df = my_driver_place(name = name)
 
         index_list = []
         for i in range(len(df)):
             tmp = "Task" + str(i+1)
             index_list.append(tmp)
-
-
         df.index = index_list
         st.dataframe(df, width=500 , height = 100)
+        st.image('img/icons/mapIcon.png')
 
         #frame = st.slider('経過時間(分)')
         #fig = my_animation(frame = frame)
@@ -77,20 +85,18 @@ if go == True :
 
     else:
 
-        st.write(f"本日の{area_option}エリアの配達")
+        st.info(f"本日の{area_option}エリアの配達")
         fig = my_task_view(name = "all")
         st.pydeck_chart(fig)
 
-        st.write("配送アニメーション")
+        st.success("配送アニメーション")
         st.image('all.gif')
 
         #df = pd.read_csv('data/guests_tokyo.csv')
         #df = df.drop(['ゲスト番号','希望時間帯'],axis = 1)
         #st.dataframe(df.head(20), width = 1000000)
 
-
-
-        st.write("時間を指定した顧客の数を変更した時のシミュレーション結果")
+        st.error("時間を指定した顧客の数を変更した時のシミュレーション結果")
         fig = my_CO2_chart()
         st.plotly_chart(fig)
 
