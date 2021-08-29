84556433bb825d0b3a1b8a7370f74c34eb2bb097
sergicalsix <kenp3434membertip1@gmail.com>
2021-08-29 12:26:37 +0900

    add: animation'

--- animation.py
+++ animation.py
@@ -6,10 +6,10 @@ import pydeck as pdk
 from typing import List,Dict
 import datetime
 
-import streamlit as st
+import streamlit as st #テスト用
 
 #df = pd.read_csv('data/results.csv')
-def my_gantt_chart(name:str , area_option = "Tokyo") -> object:
+def my_gantt_chart(name:str = "大久保" , area_option = "Tokyo") -> object:
 
     if area_option == "Tokyo":
         move_seconds:list = [[9, 11, 5, 11], [9, 12, 8, 11, 6], [8, 5, 6, 8, 3, 9, 3], [6, 9, 5, 11, 8], [6, 3, 5, 6]]
@@ -57,14 +57,27 @@ def my_gantt_chart(name:str , area_option = "Tokyo") -> object:
     #fig.show()
     return fig
 
-def my_task_view(name:str, area_option = "Tokyo") -> object:
+def my_task_view(name:str = "大久保", area_option = "Tokyo") -> object:
     """
+    nameが指定された文字以外の場合は、全員表示
+
     out:pdk.Deck
     """
     if area_option == "Tokyo":
         df = pd.read_csv('data/guests_tokyo.csv')
         df_yamato = pd.read_csv('data/host_tokyo.csv')
+        visited_places = {1: [0, 2, 5, 16, 0], 2: [0, 3, 6, 11, 17, 0], 3: [0, 4, 1, 7, 14, 15, 19, 0], 4: [0, 8, 13, 12, 18, 0], 5: [0, 10, 9, 20, 0]}
         x,y = 35.66062359622924,139.704968885932
+
+    # 運転手が訪問する場所の適用
+    name_to_num:dict = {'大久保':0,'Chou':1,'渋谷':2,'渡辺':3,'高橋':4}
+    if name in name_to_num:
+        key_ = name_to_num[name] + 1
+        visited_place:list = visited_places[key_]
+        #visited_places.remove(0)
+        #visited_places.remove(0)
+
+        df = df[df["ゲスト番号"].isin(visited_place)]
 
 
     layers=[
@@ -93,6 +106,8 @@ def my_task_view(name:str, area_option = "Tokyo") -> object:
                 latitude= x,
                 longitude= y,
                 zoom=14,
+                max_zoom = 16,
+                min_zoom = 8,
                 pitch=50,
             ),
             layers = layers
@@ -122,3 +137,113 @@ def my_CO2_chart() -> object:
     fig = go.Figure(data=traces,layout=layout)
 
     return fig
+
+
+def my_animation(frame = 0) -> object:
+    """
+    アニメーションはこの関数で表示しようかな〜
+    frameは親で処理する。
+
+    """
+    x,y = 35.66062359622924,139.704968885932
+    df = pd.read_csv('data/guests_tokyo.csv')
+    df_yamato = pd.read_csv('data/host_tokyo.csv')
+
+    df_frame = my_make_frame(total_frame = 60,area_option = "Tokyo")
+    df_frame = df_frame.iloc[frame,:].T
+
+    layers=[
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df,
+            get_position='[経度, 緯度]',
+            #get_fill_color="ward_color",
+            get_fill_color=[180, 0, 200,150],
+            get_radius=30,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df_yamato,
+            get_position='[経度, 緯度]',
+            #get_fill_color="ward_color",
+            get_fill_color=[55, 200, 255,150],
+            get_radius=50
+        )
+       ]
+
+
+    fig = pdk.Deck(
+            map_style='mapbox://styles/mapbox/streets-v11',
+            initial_view_state=pdk.ViewState(
+                latitude= x,
+                longitude= y,
+                zoom=14,
+                max_zoom = 14,
+                min_zoom = 8,
+                pitch=50,
+            ),
+            layers = layers
+            )
+
+    return fig
+
+
+def my_make_frame(total_frame = 60,area_option = "Tokyo") -> object:
+    """
+    アニメーション用のフレームを作る.
+    60分で、移動時間の最小値が3だがプログラムの都合上
+    1フレーム1分とする
+
+    out: 5(トラック)×60のデータフレーム <- グラフを描画しやすいから
+    """
+    if area_option == "Tokyo":
+        pos_df = pd.read_csv('data/guests_tokyo.csv')
+        visited_places = {1: [0, 2, 5, 16, 0], 2: [0, 3, 6, 11, 17, 0], 3: [0, 4, 1, 7, 14, 15, 19, 0], 4: [0, 8, 13, 12, 18, 0], 5: [0, 10, 9, 20, 0]}
+        move_seconds:list = [[9, 11, 5, 11], [9, 12, 8, 11, 6], [8, 5, 6, 8, 3, 9, 3], [6, 9, 5, 11, 8], [6, 3, 5, 6]]
+        name_to_num:dict = {'大久保':0,'Chou':1,'渋谷':2,'渡辺':3,'高橋':4}
+        x,y = 35.66062359622924,139.704968885932
+
+    #ひとまず大久保さんのデータのみを作ることに注力
+
+
+    name = "大久保"
+    #if name in name_to_num:
+    key_ = name_to_num[name]
+    move_second:list = move_seconds[key_]
+    visited_place:list = visited_places[key_ + 1]
+
+    #print(move_second, visited_place)
+    pos_x, pos_y = [x], [y]
+    pre_x, pre_y = x,y
+    for i , v in enumerate( visited_place[1:]):
+        if v == 0:
+            next_x, next_y = x,y
+        else:
+            place_ = pos_df[pos_df["ゲスト番号"] == v]
+            next_x, next_y = place_["緯度"].values[0], place_["経度"].values[0]
+        #print(v)
+        #print(next_x)
+        time:int = move_second[i]
+
+        delta_x = ( next_x - pre_x ) / time
+        delta_y = ( next_y - pre_y ) / time
+
+        for _ in range(time):
+            pre_x += delta_x
+            pre_y += delta_y
+
+            pos_x.append(pre_x)
+            pos_y.append(pre_y)
+
+    #休憩の追加
+    for _ in range(61 - len(pos_x)):
+        pos_x.append(pre_x)
+        pos_y.append(pre_y)
+
+
+    #    pre_x, pre_y = next_x , next_y
+    df = pd.DataFrame()
+    df["経度"] = pos_x
+    df["緯度"] = pos_y
+
+    return df
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from animation import *
-
+import time
 
 st.set_page_config(
    page_title="超スケジューリングくん",
@@ -44,9 +44,10 @@ st.markdown(
 #area_option
 
 
-go = st.button('実行!!')
+go = st.sidebar.checkbox('実行!!')
 # ここは変える、
 if go == True :
+
     #st.image('test.gif')
 
     if user_option == '運転手':
@@ -54,14 +55,28 @@ if go == True :
         fig = my_gantt_chart(name = name)
         st.plotly_chart(fig)
         #地図の描画
+
+        st.write(f"{name}さんに配達をお願いしている場所です！本日はよろしくお願いいたします！")
         fig = my_task_view(name = name)
         st.pydeck_chart(fig)
+
+        #st.write("配送アニメーション")
+
+        #frame = st.slider('経過時間(分)')
+        #fig = my_animation(frame = frame)
+        #st.pydeck_chart(fig)
+        #time.sleep(1)
 
     else:
         fig = my_CO2_chart()
         st.plotly_chart(fig)
-        fig = my_task_view(name = "hoge")
+        st.write(f"本日の{area_option}エリアの配達")
+        fig = my_task_view(name = "all")
         st.pydeck_chart(fig)
+
+        st.write("配送アニメーション")
+        st.image(all.gif)
+
 
 
 st.markdown(
